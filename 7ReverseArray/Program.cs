a62e01225548807fe0c8020bbe71084dc509c5e2
Greg Smith <gregsmith446@gmail.com>
2019-01-22 11:00:24 -0500

    completed

--- 7ReverseArray/Program.cs
+++ 7ReverseArray/Program.cs
@@ -10,6 +10,21 @@ namespace _7ReverseArray
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("size of array - int");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter {0} space separated integers", n);
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            Array.Reverse(arr);
+
+            foreach (int num in arr)
+            {
+                Console.Write("{0} ", num);
+            }
+
+
+
         }
     }
 }
